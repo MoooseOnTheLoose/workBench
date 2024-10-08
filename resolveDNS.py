b869c3b349ce85fa539cc6c0332cbbd1ca1c1996
Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>
2024-10-08 15:47:45 -0400

    Create resolveDNS.py
    Signed-off-by: Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>

---
+++ resolveDNS.py
@@ -0,0 +1,22 @@
+import dns.resolver #pip3 install dnspython
+'''    
+Records 
+MAIL SERVERS = MX
+NAMER SERVERS = NS
+IPv4 ADDRESS = A
+IPv6 ADDRESS = AAA
+'''
+def main():
+    resolveDNS()    
+def resolveDNS():
+    hosts = ["google.com", "microsoft.com"]
+    for host in hosts:
+        print(host)
+        ip = dns.resolver.resolve(host, "A") 
+        for i in ip:
+            print(i)      
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
