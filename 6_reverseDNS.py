eff92b24768dfaea486981f1297f17b242a06942
Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>
2024-10-09 10:55:46 -0400

    Update 6_reverseDNS.py
    Signed-off-by: Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>

--- 6_reverseDNS.py
+++ 6_reverseDNS.py
@@ -2,9 +2,11 @@ import dns.reversename #pip3 install dnspython
 def main():
     reverseDNS()
 def reverseDNS():
-    domain = dns.reversename.from_address("172.253.63.100") # Insert target address here
+    domain = dns.reversename.from_address("142.250.31.101")
+    print("-"*36)
     print(domain)
-    print(dns.reversename.to_address(domain))  
+    print(dns.reversename.to_address(domain))
+    print("-"*36)  
 if __name__ == "__main__":
     try:
         main()
