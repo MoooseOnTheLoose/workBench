0884cdb4de69bb97ec2fd9048094e8562b2b65e7
Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>
2024-10-09 10:52:06 -0400

    Update 7_resolveDNS.py
    Signed-off-by: Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>

--- 7_resolveDNS.py
+++ 7_resolveDNS.py
@@ -12,9 +12,11 @@ def resolveDNS():
     hosts = ["google.com", "microsoft.com"]
     for host in hosts:
         print("\n" + host)
+        print("-" * 36)
         ip = dns.resolver.resolve(host, "AAAA") 
         for i in ip:
-            print(i)      
+            print(i) 
+        print("-" * 36)     
 if __name__ == "__main__":
     try:
         main()
