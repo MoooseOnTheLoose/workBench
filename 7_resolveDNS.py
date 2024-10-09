7086be91ad730858636554fe9c91238969e41a53
Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>
2024-10-09 10:41:16 -0400

    Update 7_resolveDNS.py
    Signed-off-by: Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>

--- 7_resolveDNS.py
+++ 7_resolveDNS.py
@@ -4,15 +4,15 @@ Records
 MAIL SERVERS = MX
 NAMER SERVERS = NS
 IPv4 ADDRESS = A
-IPv6 ADDRESS = AAA
+IPv6 ADDRESS = AAAA
 '''
 def main():
     resolveDNS()    
 def resolveDNS():
-    hosts = ["google.com", "microsoft.com"] # Insert target host here
+    hosts = ["google.com", "microsoft.com"]
     for host in hosts:
         print("\n" + host)
-        ip = dns.resolver.resolve(host, "NS") # Insert desired record here 
+        ip = dns.resolver.resolve(host, "AAAA") 
         for i in ip:
             print(i)      
 if __name__ == "__main__":
