666b3c20befff4d31f417bf00593eaebf9d5b4f6
Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>
2026-01-09 09:11:44 -0500

    Update 4_resolveDNS.py
    Signed-off-by: Mooose <141202156+MoooseOnTheLoose@users.noreply.github.com>

--- 4_resolveDNS.py
+++ 4_resolveDNS.py
@@ -1,24 +1,44 @@
-import dns.resolver #pip3 install dnspython
-'''    
-Records 
-MAIL SERVERS = MX
-NAMER SERVERS = NS
-IPv4 ADDRESS = A
-IPv6 ADDRESS = AAAA
-'''
-def main():
-    resolveDNS()    
-def resolveDNS():
-    hosts = ["google.com", "microsoft.com"]
+import dns.resolver
+import dns.exception
+
+def resolve_dns(hosts, record_types=("A", "AAAA"), timeout=2.0, lifetime=5.0):
+    resolver = dns.resolver.Resolver()
+    resolver.timeout = timeout
+    resolver.lifetime = lifetime
+
+    results = {}
     for host in hosts:
-        print("\n" + host)
+        host_res = {}
+        for rtype in record_types:
+            try:
+                answers = resolver.resolve(host, rtype)
+                host_res[rtype] = [str(rdata) for rdata in answers]
+            except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+                host_res[rtype] = []
+            except (dns.resolver.Timeout, dns.resolver.NoNameservers) as e:
+                host_res[rtype] = {"error": type(e).__name__}
+            except dns.exception.DNSException as e:
+                host_res[rtype] = {"error": type(e).__name__}
+        results[host] = host_res
+    return results
+
+def main():
+    hosts = ["google.com", "microsoft.com"]
+    data = resolve_dns(hosts, record_types=("AAAA",))
+    for host, host_res in data.items():
+        print(f"\n{host}\n" + "-" * 36)
+        v = host_res.get("AAAA", [])
+        if isinstance(v, dict) and "error" in v:
+            print(f"AAAA error: {v['error']}")
+        elif v:
+            for item in v:
+                print(item)
+        else:
+            print("No AAAA records")
         print("-" * 36)
-        ip = dns.resolver.resolve(host, "AAAA") 
-        for i in ip:
-            print(i) 
-        print("-" * 36)     
+
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        exit()
+        raise SystemExit(0)
